2cccf3ec5fbdb8078fdf8603c97002da87d262c7
Tomas <tomasg88@gmail.com>
2018-07-01 19:12:47 -0300

    Delete isFirefox key from global

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,4 @@
 angular.module('avanti')
 .constant('BROWSER', {
-  isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
-  isFirefox: /firefox/i.test(navigator.userAgent)
+  isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 });
