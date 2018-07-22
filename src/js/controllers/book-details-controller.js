1e737abb438dd1af7ee47d039355bf015a4df47e
Tomas Gonzalez <tomasg88@gmail.com>
2018-07-22 20:50:30 -0300

    Remove css from controller, enhance styles

--- src/js/controllers/book-details-controller.js
+++ src/js/controllers/book-details-controller.js
@@ -13,31 +13,9 @@ angular.module('avanti').controller('bookDetailsCtlr',
 
         /* PRIVATE FUNCTIONS */
         function init() {
-
             $scope.bookName = $routeParams.bookId.toUpperCase();
-
-            $scope.specialStyles = {
-                contenido: {
-                    lineHeight: 1.4,
-                    color: "#B2B2B2",
-                    marginTop: 0,
-                    marginBottom: 0
-                },
-                tituloItem: {
-                    lineHeight: 1.4,
-                    color: "#B2B2B2",
-                    marginTop: 0,
-                    marginBottom: 0,
-                    paddingTop: '5%'
-                },
-                propiedadesLibro: {
-                    color: "#B2B2B2"
-                }
-            };
         }
-        /* PUBLIC FUNCTIONS */
-
-
+        
         // Initialize
         init();
     }
