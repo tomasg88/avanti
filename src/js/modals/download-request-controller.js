85b0545b866346e11fe78daa66a41d5df6bafd2b
Tomas <tomasg88@gmail.com>
2019-03-22 12:16:22 -0300

    Moving on with download

--- src/js/modals/download-request-controller.js
+++ src/js/modals/download-request-controller.js
@@ -23,24 +23,65 @@ angular.module('avanti').controller('DownloadRequestModalController',
 
             if ($scope.user.name.trim() === "" || $scope.user.email.trim() === "") {
                 $scope.hasMessage = true;
-                $scope.messageText = 'CONTACT.MESSAGES.MANDATORY_FIELD';
+                $scope.messageText = 'DOWNLOADS.FORM.VALIDATION_MSGS';
                 return false;
             }
 
             if (!emailRegEx.test($scope.user.email.trim())) {
                 $scope.hasMessage = true;
-                $scope.messageText = 'CONTACT.MESSAGES.EMAIL_FORMAT';
+                $scope.messageText = 'DOWNLOADS.FORM.VALIDATION_MSGS';
                 return false;
             }
             return true;
+        }
+
+        function doDownload() {
+			// Trick to download images in Firefox
+			HTMLElement.prototype.click = function() {
+				var evt = this.ownerDocument.createEvent('MouseEvents');
+				evt.initMouseEvent('click', true, true, this.ownerDocument.defaultView, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+				this.dispatchEvent(evt);
+			};
+			var anchor;
+			var chrome   = navigator.userAgent.indexOf('Chrome') > -1;
+			var explorer = navigator.userAgent.indexOf('MSIE') > -1;
+			var safari   = navigator.userAgent.indexOf('Safari') > -1;
+			var opera    = navigator.userAgent.toLowerCase().indexOf('op') > -1;
+			if ((chrome) && (safari)) {safari = false;}
+			if ((chrome) && (opera)) {chrome = false;}
+
+			if (safari || explorer) {
+				anchor = anchor || angular.element([
+					'<a',
+					' href="',
+					$scope.content,
+					'" target="_blank"></a>'
+				].join(''))[0];
+			} else {
+				anchor = anchor || angular.element([
+					'<a download="',
+					getDownloadTitle(),
+					'.',
+					$scope.extension,
+					'" href="',
+					$scope.content,
+					'" target="_blank"></a>'
+				].join(''))[0];
+			}
+
+            $timeout(function() {
+                anchor.click();
+            }, 0);
+		}
+
+        function getDownloadTitle() {
+
         }
 
         $scope.ok = function() {
             if (validateForm()) {
                 Email.sendEmail($scope.user).then(
                     function onSuccess(response) {
-                        $scope.hasMessage = true;
-                        $scope.messageText = 'DOWNLOADS.MESSAGES.EMAIL_SENT';
                         doDownload();
                         $scope.close();
                     },
